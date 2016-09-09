5feddaa0542b4066c67a89e71ab327a96187f809
jbudge3 <jake.budge@me.com>
2016-09-08 18:29:31 -0600

    Completed instance array

--- instanceArray.js
+++ instanceArray.js
@@ -1,5 +1,5 @@
 /*
-  Your 3 Users will be the following. 
+  Your 3 Users will be the following.
     0) Tyler, tylermcginnis33@gmail.com, 'iLoveJS'
     1) Cahlan, cahlan@devmounta.in, 'iLoveHashtags'
     2) Lenny, lenny@theLenster.com, 'iLoveLentilSoup'
@@ -15,17 +15,28 @@ var User = function(name, email, pw){
 
   //code here
 
+  var users = [];
+
 
 //Now create and push into your users array 3 separate instances of User using the data from above in that exact order
 
   //code here
 
-console.log('Tyler\'s information is ');
+  var tyler = new User('Tyler', 'tylermcginnis33@gmail.com', 'iLoveJS');
+  users.push(tyler);
+
+  var cahlan = new User('Cahlan', 'cahlan@devmounta.in', 'iLoveHashTags');
+  users.push(cahlan);
+
+  var lenny = new User('Lenny', 'lenny@theLenster.com', 'iLoveLentilSoup');
+  users.push(lenny);
+
+console.log('Tyler\'s information is ' + users[0]);
 //Console.log all of Tylers information
 
   //code here
 
-console.log('Lenny\'s information is ');
+console.log('Lenny\'s information is ' + users[2]);
 //Now console.log all of Lennys information
 
   //code here
@@ -35,7 +46,18 @@ console.log('Lenny\'s information is ');
 
   //code here
 
+  var jake = new User('Jake', 'jake.budge@me.com', 'iLoveLynsey');
+  users.push(jake);
+
 console.log('All my users names are ');
-//Now loop through your users Array and console.log every users name. 
+//Now loop through your users Array and console.log every users name.
 
   //code here
+
+  function getUsersNames(arr) {
+    for (var i = 0; i < arr.length; i++) {
+      console.log(arr[i]['name']);
+    }
+  }
+
+  getUsersNames(users);
